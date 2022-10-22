7a3eee840c1f70d3efbe1d52e1fd88c967c74434
Jonatan Morales <developerjonatanmorales@gmail.com>
2022-10-21 18:46:49 -0600

    simi terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,76 @@
-const themeLight = 'Theme light';
-const themeDark = 'Theme dark';
+const themeLight = "Tema claro";
+const themeDark = "Tema oscuro";
 
-let btnTheme = document.getElementById('btnTheme');
-let iconTheme = document.getElementById('iconTheme');
-let isBtnCheckbox = document.getElementById('btnCheckbox');
+/*
+ *btn temas
+ */
+let btnTheme = document.getElementById("btnTheme");
+let iconTheme = document.getElementById("iconTheme");
+let isBtnCheckbox = document.getElementById("btnCheckbox");
 
-let btnMenu = document.getElementById('btnMenu');
-let btnCheckboxMenu = document.getElementById('checkboxMenu');
-let items = document.getElementById('items');
+/*
+ *btn menu
+ */
+let btnMenu = document.getElementById("btnMenu");
+let btnCheckboxMenu = document.getElementById("checkboxMenu");
+let items = document.getElementById("items");
 
-btnTheme.addEventListener('click', () => {
-    
-    document.getElementById('body').classList.toggle('theme');
+/*
+ *btn acordion
+ */
+const leguajes = document.getElementById("leguajes");
+const DB = document.getElementById("DB");
+const frameworks = document.getElementById("frameworks");
+const CV = document.getElementById("CV");
 
-    if (isBtnCheckbox.checked == true) {
-        // btnTheme.innerText = themeLight;
-        document.getElementById("iconMenu").style.filter = "invert(1)"
-        iconTheme.classList.remove('fa-moon')
-        iconTheme.classList.add('fa-sun')
-    } else {
-        // btnTheme.innerText = themeDark;
-        document.getElementById("iconMenu").style.filter = "invert(0)"
-        iconTheme.classList.remove('fa-sun')
-        iconTheme.classList.add('fa-moon')
-    }
+btnTheme.addEventListener("click", () => {
+  document.getElementById("body").classList.toggle("theme");
 
+  if (isBtnCheckbox.checked == true) {
+    btnTheme.innerText = themeLight;
+    iconTheme.classList.remove("fa-moon");
+    iconTheme.classList.add("fa-sun");
+
+  } else {
+    btnTheme.innerText = themeDark;
+    iconTheme.classList.remove("fa-sun");
+    iconTheme.classList.add("fa-moon");
+
+  }
 });
 
-btnMenu.addEventListener('click', () => {
-    if (btnCheckboxMenu.checked == true) {
-        items.classList.add('navbar__items-active');
-    }else {
-        items.classList.remove('navbar__items-active');
+btnMenu.addEventListener("click", () => {
+  if (btnCheckboxMenu.checked == true) {
+    items.classList.add("navbar__items-active");
+  } else {
+    items.classList.remove("navbar__items-active");
+  }
+});
 
-    }
-})
+leguajes.addEventListener("click", () => {
+  document
+    .getElementById("leguaje__chil")
+    .classList.toggle("nav__children-active");
+  document
+    .getElementById("icon__leguajes")
+    .classList.toggle("nav__icon-active");
+});
+
+DB.addEventListener("click", () => {
+  document.getElementById("DB__chil").classList.toggle("nav__children-active");
+  document.getElementById("icon__db").classList.toggle("nav__icon-active");
+});
+
+frameworks.addEventListener("click", () => {
+  document
+    .getElementById("frameworks__chil")
+    .classList.toggle("nav__children-active");
+  document
+    .getElementById("icon__framework")
+    .classList.toggle("nav__icon-active");
+});
+
+CV.addEventListener("click", () => {
+  document.getElementById("CV__chil").classList.toggle("nav__children-active");
+  document.getElementById("icon__cv").classList.toggle("nav__icon-active");
+});
